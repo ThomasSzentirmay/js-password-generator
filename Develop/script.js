13c8bf75fa4f5e774042ada184eeb86d09092591
Thomas Szentirmay <tlszent@outlook.com>
2023-06-02 22:06:05 -0400

    re-arranged starter JS code so that it is formatted for the code that we have learnt in class

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+
 
   passwordText.value = password;
 
