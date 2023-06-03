2699877c70b4760174fd3d2374eff3d4e0f93703
Thomas Szentirmay <tlszent@outlook.com>
2023-06-02 22:16:30 -0400

    created userInput function and created variables to hold the user input amount and booleans

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,29 @@ var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '<', '>', ',', '.', '?', '/'];
 
+function (userInput) {
+
+// we store the return value from the user in variables (amount, includeLowercase)
+
+  while(true) {
+    var amount = parseInt(prompt('Please provide charcter length'));
+  if (amount < 8 || amount > 128) {
+    alert('Please enter a character length between 8 and 128!')
+    continue;
+  }
+  var includeLowercase = confirm('Click OK to include lowercase letters')
+  var includeUppercase = confirm('Click OK to include uppercase lertters')
+  var includeNumbers = confirm('Click OK to include numbers')
+  var includeSpecial = confirm('Click OK to include special characters')
+  break;
+  }
+
+  // return the value of the userInput function into an array
+
+  return [amount, includeLowercase, includeUppercase, includeNumbers, includeSpecial]
+
+}
+
 function writePassword() {
   
 
